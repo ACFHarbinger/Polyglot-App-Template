b271d107b495f1e76566be3f7e06656cd81a43c2
ACFHarbinger <afonso.fernandes100@gmail.com>
2026-08-13 00:42:03 +0100

    chore(infra): relocate cloud provider prototypes

--- infra/protos/firebase/firebase_config.js
+++ infra/protos/firebase/firebase_config.js
@@ -1,4 +1,4 @@
-// libraries/app/firebase/firebase_config.js
+// infra/protos/firebase/firebase_config.js
 // Firebase configuration and modular SDK initialization example.
 
 import { initializeApp } from "firebase/app";
